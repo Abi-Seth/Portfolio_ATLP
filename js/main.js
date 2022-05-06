2319a3627cea6b1186f02a70fb190bfb07538c2d
abi-seth <abiheloaf@gmail.com>
2022-05-06 16:35:03 +0200

    [feat]: finished necessary integration

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ function toggleMenu() {
         toggled = true;
     }
 }
+
+function checkAuth() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'unset';
+    const token = localStorage.getItem('auth_token');
+
+    if (
+        window.location.href.split('//')[1].split('/')[1] == 'login.html' ||
+        window.location.href.split('//')[1].split('/')[1] == 'dashboard.html' ||
+        window.location.href.split('//')[1].split('/')[1] == 'messages.html' ||
+        window.location.href.split('//')[1].split('/')[1] == 'deleteblog.html' ||
+        window.location.href.split('//')[1].split('/')[1] == 'editblog.html' ||
+        window.location.href.split('//')[1].split('/')[1] == 'addblog.html'
+    ) {
+        if (token == null) {
+            if (window.location.href.split('//')[1].split('/')[1] != 'login.html')
+                window.location = './login.html';
+        } else {
+            if (window.location.href.split('//')[1].split('/')[1] == 'login.html')
+                window.location = './dashboard.html';
+        }
+        loader.style.display = 'none';
+    } else {
+        loader.style.display = 'none';
+    }
+}
+checkAuth();
